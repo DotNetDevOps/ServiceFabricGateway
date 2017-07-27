a2fe2a323fc895c382ea025b8b655dee23bed6df
Poul Kjeldager Sørensen <pks@s-innovations.net>
2017-07-27 16:18:39 +0200

    target net462 and aspnetcore2 for storage

---
+++ apps/S-Innovations.ServiceFabric.GatewayService/Configuration/KeyVaultSecretManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Logging;
+using SInnovations.ServiceFabric.Storage.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.ServiceFabric.GatewayService.Configuration
+{
+    public class KeyVaultSecretManager : IKeyVaultSecretManager
+    {
+        private readonly ILogger Logger;
+        private readonly AzureADConfiguration AzureAD;
+        public string KeyVaultUrl { get; set; }
+        public KeyVaultClient Client { get; set; }
+
+        public KeyVaultSecretManager(
+          ConfigurationPackage configurationPackage,
+          AzureADConfiguration AzureAd,
+          ILoggerFactory logFactory)
+        {
+            this.Logger = logFactory.CreateLogger<StorageConfiguration>();
+            this.AzureAD = AzureAd;
+
+            var section = configurationPackage.Settings.Sections["AzureResourceManager"].Parameters;
+            KeyVaultUrl = section["Azure.KeyVault.Uri"].Value;
+
+            KeyVaultClient.AuthenticationCallback callback =
+                (authority, resource, scope) => AzureAD.GetTokenFromClientSecret(authority, resource);
+
+            Client = new KeyVaultClient(callback);
+        }
+
+
+        /// <inheritdoc />
+        public virtual string GetKey(SecretBundle secret)
+        {
+
+            return "KeyVault:" + secret.SecretIdentifier.Name.Replace("--", ConfigurationPath.KeyDelimiter);
+        }
+
+        /// <inheritdoc />
+        public virtual bool Load(SecretItem secret)
+        {
+
+            return true;
+        }
+
+    }
+}
--- apps/S-Innovations.ServiceFabric.GatewayService/Configuration/StorageConfiguration.cs
+++ apps/S-Innovations.ServiceFabric.GatewayService/Configuration/StorageConfiguration.cs
@@ -78,46 +78,7 @@ namespace SInnovations.ServiceFabric.GatewayService.Configuration
         public string DnsMadeEasyCredentials { get; set; }
     }
 
-    public class KeyVaultSecretManager : IKeyVaultSecretManager
-    {
-        private readonly ILogger Logger;
-        private readonly AzureADConfiguration AzureAD;
-        public string KeyVaultUrl { get; set; }
-        public KeyVaultClient Client { get; set; }
-
-        public KeyVaultSecretManager(
-          ConfigurationPackage configurationPackage,
-          AzureADConfiguration AzureAd,
-          ILoggerFactory logFactory)
-        {
-            this.Logger = logFactory.CreateLogger<StorageConfiguration>();
-            this.AzureAD = AzureAd;
-
-            var section = configurationPackage.Settings.Sections["AzureResourceManager"].Parameters;
-            KeyVaultUrl = section["Azure.KeyVault.Uri"].Value;
-
-            KeyVaultClient.AuthenticationCallback callback =
-                (authority, resource, scope) => AzureAD.GetTokenFromClientSecret(authority, resource);
-
-            Client = new KeyVaultClient(callback);
-        }
-
-        
-        /// <inheritdoc />
-        public virtual string GetKey(SecretBundle secret)
-        {
-         
-            return "KeyVault:"+ secret.SecretIdentifier.Name.Replace("--", ConfigurationPath.KeyDelimiter);
-        }
-
-        /// <inheritdoc />
-        public virtual bool Load(SecretItem secret)
-        {
-          
-            return true;
-        }
-
-    }
+    
   
     
 
