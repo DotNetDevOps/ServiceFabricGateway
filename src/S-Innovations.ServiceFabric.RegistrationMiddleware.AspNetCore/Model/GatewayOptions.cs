cefe6c10d455aa160fd3a6829451df280361a55f
Poul Kjeldager Sørensen <pks@s-innovations.net>
2018-04-04 22:03:30 +0200

    fixed gateway type

--- src/S-Innovations.ServiceFabric.RegistrationMiddleware.AspNetCore/Model/GatewayOptions.cs
+++ src/S-Innovations.ServiceFabric.RegistrationMiddleware.AspNetCore/Model/GatewayOptions.cs
@@ -2,13 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SInnovations.ServiceFabric.RegistrationMiddleware.AspNetCore.Model
 {
-   
-    public class GatewayOptions
+
+    [DataContract]
+    [KnownType(typeof(string[]))]
+    public class GatewayOptions : IExtensibleDataObject
     {
         public string Key { get; set; }
         public string ReverseProxyLocation { get; set; }
@@ -17,6 +20,14 @@ namespace SInnovations.ServiceFabric.RegistrationMiddleware.AspNetCore.Model
         public ProxyPassCacheOptions CacheOptions { get; set; } = new ProxyPassCacheOptions();
 
         public Dictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
-       
+
+        private ExtensionDataObject theData;
+
+        public virtual ExtensionDataObject ExtensionData
+        {
+            get { return theData; }
+            set { theData = value; }
+        }
+
     }
 }
