faeb14a50f98a039e98ed70fb3ecc93246235f4b
Poul Kjeldager Sørensen <pks@s-innovations.net>
2019-03-10 12:50:43 +0100

    refactored and added provider routes

---
+++ src/S-Innovations.ServiceFabric.ResourceProvider/Attributes/ResourceProviderRouteAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.ServiceFabric.ResourceProvider.Attributes
+{
+    public class ResourceProviderRouteAttribute : RouteAttribute
+    {
+        public const string ResourceGroupsRoutingParameter = "resourcegroups";
+        public const string SubscriptionsRoutingParameter = "subscriptions";
+        public const string ProviderRoutingParameter = "providers";
+
+        public ResourceProviderRouteAttribute(string provider, string route, bool withSubscriptionRouting = true, bool withResourceGroupRouting = true, string subscriptionsRoutingParameter = SubscriptionsRoutingParameter, string resourceGroupsRoutingParameter = ResourceGroupsRoutingParameter, string providerRoutingParameter = ProviderRoutingParameter)
+            : base($"{(withSubscriptionRouting ? $"{subscriptionsRoutingParameter}/{{subscriptionId}}" : "")}{(withResourceGroupRouting ? $"/{resourceGroupsRoutingParameter}/{{resourceGroupName}}" : "")}/{providerRoutingParameter}/{provider}/{route}".Trim('/'))
+        {
+
+        }
+
+
+    }
+    public class ProviderRouteAttribute : ResourceProviderRouteAttribute
+    {
+        public ProviderRouteAttribute(string provider, string route) : base(provider, route, false, false)
+        {
+        }
+
+
+    }
+    public class ProviderAttribute : ProviderRouteAttribute
+    {
+        public ProviderAttribute(string provider) : base(provider, "")
+        {
+        }
+    }
+}
